3c04d94096067bf112a0a4a8ec230670fc59a603
kynaios <blaise.witczak@gmail.com>
2024-02-20 20:18:51 +0100

    Create commands and handlers abstraction

---
+++ Shiptech.Shared.Abstractions/Commands/ICommand.cs
@@ -0,0 +1,6 @@
+namespace Shiptech.Shared.Abstractions.Commands
+{
+    public interface ICommand
+    {
+    }
+}
---
+++ Shiptech.Shared.Abstractions/Commands/ICommandDispatcher.cs
@@ -0,0 +1,7 @@
+namespace Shiptech.Shared.Abstractions.Commands
+{
+    public interface ICommandDispatcher
+    {
+        Task DispatchAsync<TCommand>(TCommand command) where TCommand : class, ICommand;
+    }
+}
