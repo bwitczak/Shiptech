de56fd41796bc9a2670454af13fe05ed560a3aab
kynaios <blaise.witczak@gmail.com>
2024-03-26 21:26:10 +0100

    Remove Swagger with dependency

--- Shiptech.Api/Program.cs
+++ Shiptech.Api/Program.cs
@@ -17,8 +17,6 @@ var app = builder.Build();
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
-    app.UseSwagger();
-    app.UseSwaggerUI();
     app.ApplyMigrations();
 }
 
