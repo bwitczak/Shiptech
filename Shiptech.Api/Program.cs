09e4deae1fc8927a808e19426a6fb6b3f11498e1
kynaios <blaise.witczak@gmail.com>
2024-03-26 21:26:10 +0100

    Remove Swagger with dependency

--- Shiptech.Api/Program.cs
+++ Shiptech.Api/Program.cs
@@ -3,13 +3,9 @@ using Shiptech.Infrastructure;
 
 var builder = WebApplication.CreateBuilder(args);
 
+// Add services to the container.
 builder.Services.AddApplication();
 builder.Services.AddInfrastructure(builder.Configuration);
-
-// Add services to the container.
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
 builder.Services.AddControllers();
 
 var app = builder.Build();
@@ -17,8 +13,6 @@ var app = builder.Build();
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
-    app.UseSwagger();
-    app.UseSwaggerUI();
     app.ApplyMigrations();
 }
 
